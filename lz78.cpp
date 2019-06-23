6e9dd4938aa0d953ff806a20da36f19e8c6e82e2
Arseniy Obolenskiy <gooddoog@student.su>
2019-06-23 11:48:32 +0300

    Fix LZ78

--- lz78.cpp
+++ lz78.cpp
@@ -17,6 +17,9 @@ std::vector <LZ78::Node> LZ78::encode(const std::string &s) {
             buffer = "";
         }
     }
+    if (dict.find(buffer) != dict.end()) {
+        result.push_back({buffer != "" ? dict[buffer] : 0, '\0'});
+    }
     return result;
 }
 
@@ -24,7 +27,9 @@ std::string LZ78::decode(const std::vector <LZ78::Node> &nodes) {
     std::vector <std::string> dict = {""};
     std::string result;
     for (const LZ78::Node &node : nodes) {
-        std::string word = dict[node.pos] + node.next;
+        std::string word = dict[node.pos];
+        if (node.next)
+            word.push_back(node.next);
         result += word;
         dict.push_back(word);
     }
